fd96a50178a82c6484dd5bc426ec7637a5bd635e
Tharun Chintham <tchintha@asu.edu>
2019-01-31 22:33:14 -0700

    min and max plots of IMU

--- feature_extraction.m
+++ feature_extraction.m
@@ -54,6 +54,7 @@
 % hold on
 % plot(plot_eating_x,mean_orientation_x_eating)
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5
 
 number_of_samples = 200;
 sample_eating = floor(length(IMU_fork_eating)/number_of_samples);
@@ -65,9 +66,9 @@ IMU_fork_sampled_non_eating = [];
 
 for i = 1 : sample_eating : length(IMU_fork_eating)
     if i+sample_eating <= length(IMU_fork_eating)
-             m = mean(IMU_fork_eating(i:i+sample_eating,:));
+             m = max(IMU_fork_eating(i:i+sample_eating,:));
     else
-        m = mean(IMU_fork_eating(i:end,:));
+        m = max(IMU_fork_eating(i:end,:));
     end
    IMU_fork_sampled_eating = [IMU_fork_sampled_eating ; m];
 
@@ -77,9 +78,9 @@ end
 
 for i = 1 : sample_non_eating : length(IMU_fork_non_eating)
     if i+sample_non_eating <= length(IMU_fork_non_eating)
-             m = mean(IMU_fork_non_eating(i:i+sample_non_eating,:));
+             m = max(IMU_fork_non_eating(i:i+sample_non_eating,:));
     else
-        m = mean(IMU_fork_non_eating(i:end,:));
+        m = max(IMU_fork_non_eating(i:end,:));
     end
    IMU_fork_sampled_non_eating = [IMU_fork_sampled_non_eating ; m];
   
